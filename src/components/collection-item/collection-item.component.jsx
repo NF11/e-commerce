f000643ff91d509afd2c172187a8ec91744fe35a
Nassim Fares <nassim.fares.dz@gmail.com>
2020-08-02 02:28:50 +0100

    add transition for collectionItem component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
@@ -14,6 +15,8 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+
+    <CustomButton inverted> add to cart</CustomButton>
   </div>
 );
 
